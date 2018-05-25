84c2d1e93815f90b1caca80e2bb155fd2c6b5715
wuzhongyang <wuzhongyang@roobo.com>
2018-05-25 14:54:47 +0800

    调整model的extend配置

--- backend/models/card/card.go
+++ backend/models/card/card.go
@@ -10,7 +10,7 @@ type Card struct {
 	ImgUrl             string `xorm:"varchar(256) notnull comment('图片地址')" form:"imgUrl" json:"imgUrl"`
 
 	Configs []Config `xorm:"-"`
-	Album   `xorm:"- extends" json:"-"`
+	Album   `xorm:"extends" json:"-"`
 }
 
 func (c Card) TableName() string {
