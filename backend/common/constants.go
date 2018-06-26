62504ac4171bf9f6a97c0e822b9e8ede859d8263
wuzhongyang <wuzhongyang@roobo.com>
2018-06-26 11:03:14 +0800

    调整错误码格式

--- backend/common/constants.go
+++ backend/common/constants.go
@@ -4,15 +4,15 @@ type ErrorCode int
 
 const (
 	ErrorOk     ErrorCode = 0
-	ErrorParams ErrorCode = 1
-	ErrorFiles  ErrorCode = 2
-	ErrorMysql  ErrorCode = 3
-	ErrorRedis  ErrorCode = 4
-	ErrorAuth   ErrorCode = 5
-	ErrorServer ErrorCode = 500
+	ErrorParams ErrorCode = -1
+	ErrorAuth   ErrorCode = -401
 
-	ErrorCardIdentify ErrorCode = 1000
-	ErrorCardDetail   ErrorCode = 1001
+	ErrorCardIdentify ErrorCode = -10000
+	ErrorCardDetail   ErrorCode = -10001
+	ErrorFiles        ErrorCode = -10002
+	ErrorMysql        ErrorCode = -10003
+	ErrorRedis        ErrorCode = -10004
+	ErrorServer       ErrorCode = -10500
 )
 
 type ProjectType int
